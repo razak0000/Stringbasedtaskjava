30ee5144a7ce73dc9a3d39b7e4cf876715979be7
razak0000 <xquiringdreamer@gmail.com>
2024-11-08 10:25:12 +0530

    feat task2 added

---
+++ task2.java
@@ -0,0 +1,9 @@
+public class task2 {
+    public static void main(String[] args) {
+        String s;
+        System.out.println("enter a string :");
+        s = System.console().readLine();
+        System.out.println(s.length());
+    }
+    
+}
